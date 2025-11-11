691f444a6f34e2730e75071663c77382be43d1a0
David Wenk <david@davidwenk.de>
2025-11-11 16:04:22 +0100

    Exercise model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_11_143642) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_11_150327) do
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
